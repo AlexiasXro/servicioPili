add6cad3a3c07199c21b781ee18217d3a0c1dc1e
Reizaner <luchix1@outlook.es>
2023-06-23 21:45:37 -0300

    Botones funcionales y relink

---
+++ assets/JS/redirecciones_main.js
@@ -0,0 +1,12 @@
+function redireccionargaleria() {
+    window.location.href = 'Galeria.html';
+  }
+function redireccionareventos() {
+    window.location.href = 'Evento_tabla.html';
+  }
+function redireccionarcontacto() {
+    window.location.href = 'Contacta-formulario.html';
+  }
+  function redireccionarcarrito() {
+    window.location.href = 'Carrito_de_compras.html';
+  } 
